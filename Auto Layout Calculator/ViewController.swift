f5319eabaaf48e6e2b4bf2725bf5a73c51111898
Jose Ramos <jmrp.04@gmail.com>
2019-01-23 23:05:42 -0500

    First commit

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Jose Ramos on 7/11/18.
+//  Copyright © 2018 Jose Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var eraseButton: UIButton!
+    @IBOutlet weak var percent: UIButton!
+    @IBOutlet weak var division: UIButton!
+    @IBOutlet weak var multiplication: UIButton!
+    @IBOutlet weak var sum: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
